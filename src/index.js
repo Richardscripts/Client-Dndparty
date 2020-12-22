dfd1e97027e6c9997841298ffe56e2ba48aca2e1
unknown <richardcarmenscott@gmail.com>
2020-12-22 14:21:22 -0500

    fixed component case in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import App from './Components/App/App';
+import App from './components/App/App';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'animate.css';
